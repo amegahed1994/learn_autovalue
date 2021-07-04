08870e1718d9902831a442a0d119ca5d279d7ef9
Abdel Megahed <abdel.megahed@rdccaa.com>
2021-07-04 02:23:40 -0400

    updated Driver code

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -5,7 +5,7 @@ public class Driver {
         Example.Inner sampleValueObject = new AutoValue_Example_Inner.Builder().setAmount(12).setCurrency("usd").build();
         System.out.println(sampleValueObject);
 
-        /* In the of the code found here https://github.com/apache/beam/blob/master/sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryIO.java#L1702,
+        /* In the case of the code found [here](https://github.com/apache/beam/blob/master/sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryIO.java#L1702),
             the return type is static because this method is inside the same outer class as the AutoValue class.
          */
     }
