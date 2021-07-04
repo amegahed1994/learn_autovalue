6c4137d83555d67895bb7dc8a74778805e088b4b
Abdel Megahed <abdel.megahed@rdccaa.com>
2021-07-04 02:22:27 -0400

    updated Driver code

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,16 +1,13 @@
 public class Driver {
 
     public static void main (String args[]) {
-        /* Quickstart for working with AutoValue (which is an immutable Java value class generator).
-            The class structure of the AutoValue class in this quickstart is the same structure
-            as the one used in the Beam SDK codebase.
 
-            Having a static inner value class (i.e. non top-level) can be seen in BigQueryIO
-            [here](https://github.com/apache/beam/blob/master/sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryIO.java#L1706)
-            and in the PTransform dev guide [here](https://beam.apache.org/contribute/ptransform-style-guide/#language-neutral-considerations)
+        Example.Inner sampleValueObject = new AutoValue_Example_Inner.Builder().setAmount(12).setCurrency("usd").build();
+        System.out.println(sampleValueObject);
+
+        /* In the of the code found here https://github.com/apache/beam/blob/master/sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryIO.java#L1702,
+            the return type is static because this method is inside the same outer class as the AutoValue class.
          */
-
-        System.out.println(new AutoValue_Example_Inner.Builder().setAmount(12).setCurrency("usd").build());
     }
 }
 
