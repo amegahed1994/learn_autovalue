35e29f21d8df4d4d6e4ddd688249e175f291d7e2
Abdel Megahed <abdel.megahed@rdccaa.com>
2021-07-04 02:04:52 -0400

    modify classes

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -5,12 +5,12 @@ public class Driver {
             The class structure of the AutoValue class in this quickstart is the same structure
             as the one used in the Beam SDK codebase.
 
-            Having a static inner class (i.e. non top-level) value class can be seen in BigQueryIO
+            Having a static inner value class (i.e. non top-level) can be seen in BigQueryIO
             [here](https://github.com/apache/beam/blob/master/sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryIO.java#L1706)
             and in the PTransform dev guide [here](https://beam.apache.org/contribute/ptransform-style-guide/#language-neutral-considerations)
          */
 
-        System.out.println(new AutoValue_Main_Inner.Builder().setAmount(12).setCurrency("usd").build());
+        System.out.println(new AutoValue_Example_Inner.Builder().setAmount(12).setCurrency("usd").build());
     }
 }
 
